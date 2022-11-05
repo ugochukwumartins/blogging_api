af5e027307e94716371bd4b870110c5cc1af3e01
uokorochamartins <uokorocha@icitsolutions.com>
2022-11-05 15:34:00 +0100

    fixed

--- app.js
+++ app.js
@@ -21,9 +21,8 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(blogRoute);
 app.use(onboardingRoute);
-
+app.use(blogRoute);
 app.use(httpreq);
 
 
